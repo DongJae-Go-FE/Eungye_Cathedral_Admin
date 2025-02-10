199d38ce581f46efc185979e1ac2e013b5d445dc
가동재 <masterforce999@naver.com>
2025-02-11 04:02:40 +0900

    배포 테스트

--- src/actions/loginActions.ts
+++ src/actions/loginActions.ts
@@ -2,21 +2,24 @@
 
 import { signIn, signOut } from "@/auth";
 
+import { redirect } from "next/navigation";
+
 export async function doLogout() {
   await signOut({ redirectTo: "/login" });
 }
 
 export async function doCredentialLogin(formData: FormData) {
+  let shouldRedirect = false;
   try {
     const response = await signIn("credentials", {
       email: formData.get("email"),
       password: formData.get("password"),
-      redirect: true,
-      redirectTo: "/main",
+      redirect: false,
     });
-
+    shouldRedirect = true;
     return response;
   } catch (error) {
     console.error(error);
   }
+  if (shouldRedirect) redirect("/main");
 }
