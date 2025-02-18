cbe858e3f9b06b1f4edb05bf0acb5be2476e90b7
가동재 <masterforce999@naver.com>
2025-02-18 16:57:13 +0900

    중복 프리패칭 제거

--- src/queryApi/useListQuery.ts
+++ src/queryApi/useListQuery.ts
@@ -1,15 +1,8 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import queryOptions from "./queryOptions";
 import { ListParamsType } from "../utils/getApi";
-import { useEffect } from "react";
 
 export function useNews({ page = "1", limit = "10", search }: ListParamsType) {
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    queryClient.prefetchQuery(queryOptions.news({ page, limit, search }));
-  }, [page, limit, search, queryClient]);
-
   return useQuery(queryOptions.news({ page, limit, search }));
 }
 
@@ -18,12 +11,6 @@ export function useNotices({
   limit = "10",
   search,
 }: ListParamsType) {
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    queryClient.prefetchQuery(queryOptions.notices({ page, limit, search }));
-  }, [page, limit, search, queryClient]);
-
   return useQuery(queryOptions.notices({ page, limit, search }));
 }
 
@@ -32,11 +19,5 @@ export function useWeeklys({
   limit = "10",
   search,
 }: ListParamsType) {
-  const queryClient = useQueryClient();
-
-  useEffect(() => {
-    queryClient.prefetchQuery(queryOptions.weeklys({ page, limit, search }));
-  }, [page, limit, search, queryClient]);
-
   return useQuery(queryOptions.weeklys({ page, limit, search }));
 }
